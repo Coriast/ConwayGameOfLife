846f59b127cd980306a39893d67d0b2fba479cf9
Coriast <pedrowillianpesquisador@gmail.com>
2023-05-07 11:00:51 -0300

    aplicação das regras com sucesso.

--- src/GameOfLife.cpp
+++ src/GameOfLife.cpp
@@ -5,17 +5,19 @@
 
 using namespace std;
 
-#define GRID_S	8
+#define GRID_S	20
 #define SCR_S	600
 
-std::vector<bool> grid;
+bool grid[GRID_S][GRID_S];
 
 void interactWithCell(sf::Vector2i mouse_position, sf::Event mouse_event);
+void applyingRules();
 
 int main()
 {
-	for (int i = 0; i < (GRID_S * GRID_S); i++)
-		grid.push_back(0);
+	for (auto& v : grid)
+		for (auto& e : v)
+			e = 0;
 
 	sf::RenderWindow window(sf::VideoMode(SCR_S, SCR_S), "Conway", sf::Style::Titlebar | sf::Style::Close);
 	
@@ -34,8 +36,10 @@ int main()
 				case sf::Event::MouseButtonPressed: 
 					interactWithCell(sf::Mouse::getPosition(window), event);
 					break;
-				case sf::Event::MouseButtonReleased:
-
+				case sf::Event::KeyPressed:
+					if (event.key.code == sf::Keyboard::Space)
+						applyingRules();
+					break;
 				default:
 					break;
 			}
@@ -44,35 +48,20 @@ int main()
 		sf::RectangleShape square(sf::Vector2f(SCR_S / GRID_S, SCR_S / GRID_S));
 		vector<sf::RectangleShape> squares;
 
-		auto it = grid.cbegin();
 		for (int i = 0; i < GRID_S; i++)
 		{
 			for (int j = 0; j < GRID_S; j++)
 			{
-				if (*it == 1)
+				if (grid[i][j] == 1)
 					square.setFillColor(sf::Color(255, 255, 255));
 				else
 					square.setFillColor(sf::Color(0, 0, 0));
 
 				square.setPosition(sf::Vector2f((square.getSize().x * i), (square.getSize().y * j)));
 				squares.push_back(square);
-				
-				it++;
 			}
 		}
-		/*
-		for (int i = 0; i < GRID_S; i++)
-		{
-			for (int j = 0; j < GRID_S; j++)
-			{
-				int grid_position = (i + j) + (j * (GRID_S - 1));
-				if (grid[grid_position] == 1)
-					square.setFillColor(sf::Color(255, 255, 255));
-				else
-					square.setFillColor(sf::Color(0, 0, 0));
-			}
-		}
-		*/
+
 		window.clear(sf::Color(72, 72, 72));
 			
 		for (auto it = squares.begin(); it != squares.end(); it++)
@@ -92,14 +81,70 @@ void interactWithCell(sf::Vector2i mouse_position, sf::Event mouse_event)
 	float cell_size = SCR_S / GRID_S;
 	int cell_x = mouse_position.x / cell_size;
 	int cell_y = mouse_position.y / cell_size;
-	int position = (cell_x + cell_y) + (cell_x * (GRID_S - 1));
 
 	if (mouse_event.mouseButton.button == sf::Mouse::Left)
-		grid[position] = 1;
+		grid[cell_x][cell_y] = 1;
 	else if (mouse_event.mouseButton.button == sf::Mouse::Right)
-		grid[position] = 0;
-
-	cout << cell_x << " | " << cell_y << " | " << position << endl;
+		grid[cell_x][cell_y] = 0;
 }
 
+void applyingRules()
+{
+	bool next_grid[GRID_S][GRID_S];
+	std::fill(*next_grid, *next_grid + (GRID_S * GRID_S), 0);
+	for (int x = 0; x < GRID_S; x++)
+	{
+		for (int y = 0; y < GRID_S; y++)
+		{
+			int neighbors[8];
+			if (x == 0)
+			{
+				if (y == 0)
+				{
+					int neighbors_p[8] = { 0, grid[x + 1][y], 0, grid[x][y + 1],
+							0, 0, grid[x + 1][y + 1], 0 };
+					std::copy(neighbors_p, neighbors_p + 8, neighbors);
+				}
+				else
+				{
+					int neighbors_p[8] = { 0, grid[x + 1][y], grid[x][y - 1], grid[x][y + 1],
+							0, 0, grid[x + 1][y + 1], grid[x + 1][y - 1] };
+					std::copy(neighbors_p, neighbors_p + 8, neighbors);
+				}
+			}
+			else if (y == 0)
+			{
+				int neighbors_p[8] = { grid[x - 1][y], grid[x + 1][y], 0, grid[x][y + 1],
+							0, grid[x - 1][y + 1], grid[x + 1][y + 1], 0 };
+				std::copy(neighbors_p, neighbors_p + 8, neighbors);
+			}
+			else
+			{
+				int neighbors_p[8] = { grid[x - 1][y], grid[x + 1][y], grid[x][y - 1], grid[x][y + 1],
+							grid[x - 1][y - 1], grid[x - 1][y + 1], grid[x + 1][y + 1], grid[x + 1][y - 1] };
+				std::copy(neighbors_p, neighbors_p + 8, neighbors);
+			}
+
+			int count_neighbors = 0;
+			for (int k = 0; k < 8; k++)
+				count_neighbors += neighbors[k];
+
+			if (grid[x][y] == 1)
+			{
+				if (count_neighbors > 3)
+					next_grid[x][y] = 0;
+				else if (count_neighbors < 2)
+					next_grid[x][y] = 0;
+				else
+					next_grid[x][y] = 1;
+			}
+			else
+			{
+				if (count_neighbors == 3)
+					next_grid[x][y] = 1;
+			}
+		}
+	}
+	std::copy(&next_grid[0][0], &next_grid[0][0] + (GRID_S * GRID_S), &grid[0][0]);
+}
 
